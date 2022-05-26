e6ffb4c48f52b03e901377d02513f2065301d7c5
chrisgth <ksemkus00@gmail.com>
2022-05-26 17:36:02 +0300

    Completed homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+import mazda from '../images/mazdacx3.jpg'
+
 const Home = () => {
 	return ( 
 		<div className="home">
-			<p>arasdasd</p>
+			<div className="intro">
+				<h2>Find your next car!</h2>
+				<img src={mazda} alt="mazdacx3" />
+				<Link to="/shop">Shop Now</Link>
+			</div>
 		</div>
 	 );
 }
