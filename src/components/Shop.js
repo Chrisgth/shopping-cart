cc2fe08c0c5c79d4844465fdda6eaae6c1a01c39
chrisgth <ksemkus00@gmail.com>
2022-05-28 18:24:02 +0300

    Incorporated use of custom hook and db

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,20 @@
 import useFetch from "../hooks/useFetch";
 
 const Shop = () => {
+	
 	const {data: cars, isPending, error } = useFetch('https://chrisgth.github.io/cartapi/data/cars.json');
-	console.log(cars)
+
 	return ( 
 		<div className="shop">
-			<p></p>
+			{error && <div>{error}</div>}
+			{isPending && <div>Loading</div>}
+			<div className="shopItems">
+				{cars && cars.cars.map(car => (
+					<div className="shopItem" key={car.id}>
+						
+					</div>
+				))}
+			</div>
 		</div>
 	 );
 }
