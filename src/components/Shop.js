cb5dab37328c1097b50ba447026be44e72ca4bea
chrisgth <ksemkus00@gmail.com>
2022-05-29 12:44:19 +0300

    Remade api call to accommodate pagination

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,17 +1,45 @@
-import useFetch from "../hooks/useFetch";
+import { useEffect, useState } from "react";
 
 const Shop = () => {
-	
-	const {data: cars, isPending, error } = useFetch('https://chrisgth.github.io/cartapi/data/cars.json');
+
+	const [carsArr, setCarsArr] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(false)
+	const [currentPage, setCurrentPage] = useState(1)
+	const [postsPerPage, setPostsPerPage] = useState(4)
+
+	useEffect(() => {
+		const fetchCars = async () => {
+			setLoading(true)
+			await fetch('https://chrisgth.github.io/cartapi/data/cars.json')
+				.then((response) => {
+					if(!response.ok) {
+						throw Error('Could not fetch data')
+					}
+					return response.json()
+				})
+				.then((response) => {
+					setCarsArr(response.cars)
+					setLoading(false)
+					setError(false)
+				})
+				.catch((err) => {
+					setError(err.message)
+					setLoading(false)
+				})
+		}
+		fetchCars()
+	}, [])
+
 
 	return ( 
 		<div className="shop">
+			{loading && <div>Loading</div>}
 			{error && <div>{error}</div>}
-			{isPending && <div>Loading</div>}
 			<div className="shopItems">
-				{cars && cars.cars.map(car => (
+				{carsArr && carsArr.map(car => (
 					<div className="shopItem" key={car.id}>
-						
+						{car.engineOne}
 					</div>
 				))}
 			</div>
